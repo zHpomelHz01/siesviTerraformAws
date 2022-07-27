a87dcfe82a6d3a8ec1a9cb3db3588636744e03e1
root <root@LAP-GAMER.localdomain>
2022-07-27 02:26:46 -0500

    saving plan in file plan.txt

--- instance.tf
+++ instance.tf
@@ -1,5 +1,6 @@
+# You need to create a vars.tf file to add variables such as (AMIS,REGIONS, AWS_ACCESS_KEY and AWS_SECRET_KEY)
 resource "aws_instance" "example" {
-  ami           = lookup(var.AMIS, var.AWS_REGION, "") # last parameter is the default value
+  ami           = lookup(var.AMIS, var.AWS_REGION, "")
   instance_type = "t3.micro"
 }
 
